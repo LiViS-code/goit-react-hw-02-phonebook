5d7bc44a0c6cce9055571d94cc89a056d1c3f003
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2021-12-26 15:15:51 +0200

    add state

--- my-phonebook/src/components/ContactForm.jsx
+++ my-phonebook/src/components/ContactForm.jsx
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
 
 class ContactForm extends Component {
+  state = {
+    contacts: [],
+    name: "",
+  };
+
+  handleForm = () => {
+    return console.log("что-то ввели");
+  };
+
   render() {
     return (
       <form>
-        <input
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          placeholder="Enter contact name"
-          required
-        />
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            placeholder="Enter contact name"
+            required
+            onChange={this.handleForm}
+          />
+        </label>
       </form>
     );
   }
